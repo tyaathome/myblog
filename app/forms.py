e402248a40511c2eb30fa996348fbfe307a8ccd9
tyaathome <tyaathome@163.com>
2018-01-24 17:43:00 +0800

    push

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, length
+from wtforms.validators import DataRequired, length, email, EqualTo
 
 
 class LoginForm(FlaskForm):
-    username = StringField(u'账号', validators=[DataRequired(), length(6, 25, u'账号长度6-25')])
-    password = PasswordField(u'密码', validators=[DataRequired(), length(6, 32, u'密码长度6-32')])
+    username = StringField(u'账号', validators=[DataRequired(message=u'账号未填'), length(6, 25, u'账号长度6-25')])
+    password = PasswordField(u'密码', validators=[DataRequired(message=u'密码未填'), length(6, 32, u'密码长度6-32')])
+
+class SignForm(FlaskForm):
+    username = StringField(u'账号', validators=[DataRequired(message=u'账号未填'), length(6, 25, u'账号长度6-25')])
+    password = PasswordField(u'密码', validators=[DataRequired(message=u'密码未填'), length(6, 32, u'密码长度6-32'), EqualTo('confirm', '')])
+    confirm = PasswordField(u'确认密码')
+    email = StringField(u'邮箱', validators=[DataRequired(message=u'邮箱未填'), email(u'邮箱格式错误')])
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
-from flask import render_template, request, redirect, session, url_for
+import time
+from flask import render_template, request, redirect, session, url_for, Response, make_response, current_app
+from sqlalchemy import func
 
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, SignForm
 from app.models import User
 
 
@@ -11,31 +13,74 @@ def index():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    username = request.cookies.get('username')
+    return render_template('home.html', username = username)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    form = LoginForm(request.form)
+    if checkLogin():
+        return redirect(url_for('home'))
+    else:
+        form = LoginForm(request.form)
+        if request.method == 'POST':
+            messages = []
+            if form.validate():
+                username = form.username.data
+                password = form.password.data
+                user = User.query.filter_by(username = username).first()
+                if user != None and user.password == password:
+                    user.logincount += 1
+                    db.session.add(user)
+                    db.session.commit()
+                    response = make_response(redirect(url_for('home')))
+                    response.set_cookie('username',value=username, expires=time.time()+60)
+                    return response
+                else:
+                    messages.append('账号或密码错误')
+            else:
+                messages = [r for k, v in form.errors.items() for r in v]
+            return render_template('login.html', form = form, messages = messages)
+        return render_template('login.html', form = form)
+
+def checkLogin():
+    username = request.cookies.get('username')
+    if username:
+        return True
+    else:
+        return False
+
+@app.route('/sign', methods=['GET', 'POST'])
+def sign():
+    form = SignForm(request.form)
     if request.method == 'POST':
-        message = None
+        messages = []
         if form.validate():
             username = form.username.data
+            email = form.email.data
+            password = form.password.data
             user = User.query.filter_by(username = username).first()
-            if user != None:
-                session['username'] = username
-                user.logincount += 1
-                db.session.add(user)
-                db.session.commit()
-                return redirect(url_for('home'))
-            else:
-                message = u'用户不存在'
+            user2 = User.query.filter_by(email = email).first()
+            if user or user2 :
+                if user:
+                    messages.append('该用户已存在')
+                if user2:
+                    messages.append('该邮箱已存在')
+                return render_template('sign.html', form = form, messages = messages)
+            id = db.session.query(func.max(User.id)).scalar()
+            if not id:
+                id = 0
+            id += 1
+            user = User(id = id, username = username, email = email, password = password, logincount = 0)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login'))
         else:
-            message = form.errors['username'][0]
-            usernameMessage = form.errors['username'][0]
-            passwordMessage = form.errors['password'][0]
-            if usernameMessage:
-                message = usernameMessage
-            elif passwordMessage:
-                message = passwordMessage
-        return render_template('login.html', form = form, message = message)
-    return render_template('login.html', form = form)
+            messages = [r for k, v in form.errors.items() for r in v]
+        return render_template('sign.html', form = form, messages = messages)
+    return render_template('sign.html', form = form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie('username', value='', expires=0)
+    return response
